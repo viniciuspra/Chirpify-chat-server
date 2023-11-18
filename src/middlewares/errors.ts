c7ec3ff1855aa8fc5f7a3b9b3ef49c49c33b3d53
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-18 17:25:02 -0300

    feat: Create error handler middleware

---
+++ src/middlewares/errors.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../utils/AppError";
+
+export function errorHandler(
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+}
