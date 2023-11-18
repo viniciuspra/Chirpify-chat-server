f4c7b9404913d7c8c87a313f6fe961333905e18a
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-18 17:22:15 -0300

    feat: Integrate routes into Express server setup

--- src/http.ts
+++ src/http.ts
@@ -2,14 +2,17 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import socketio from "socket.io";
+import "express-async-errors";
 
 import { errorHandler } from "./middlewares/errors";
+import { routes } from "./routes";
 
 const app = express();
 const httpServer = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 app.use(errorHandler);
 
 const io = new socketio.Server(httpServer, {
