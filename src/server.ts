f50ebda435330f9c6f45479c5b88602d27db5710
viniciuspra <vinicius.cascaesp@gmail.com>
2023-12-08 18:47:33 -0300

    chore: Update Express server configuration to servers.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
-import { httpServer } from "./http";
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+import "express-async-errors";
+
+import { errorHandler } from "./middlewares/errors";
+import { routes } from "./routes";
+
+import { initializeSocket } from "./socket";
 
 import dotenv from "dotenv";
 dotenv.config();
 
+const app = express();
+const server = createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+app.use(errorHandler);
+
+initializeSocket(server);
+
 const PORT = process.env.PORT || 3000;
 
-httpServer.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
