fc727cefaebc13987c0c2dd92a3e380973922f7b
viniciuspra <vinicius.cascaesp@gmail.com>
2023-12-12 20:35:51 -0300

    feat: Integrate bcrypt for hashing user passwords in faker data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,30 @@
-import { prisma } from './../src/lib/prisma';
-import { faker } from '@faker-js/faker';
+import { prisma } from "./../src/lib/prisma";
+import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
 
 async function main() {
   const usersData = Array.from({ length: 10 }).map(() => ({
     fullname: faker.person.fullName(),
     username: faker.internet.userName(),
-    password: "123456",
+    password: bcrypt.hashSync("123456", 10),
     avatar: faker.image.avatar(),
-  }))
+  }));
 
   for (const userData of usersData) {
     await prisma.user.upsert({
-      where: { username: userData.username},
+      where: { username: userData.username },
       update: {},
-      create: userData
-    })
+      create: userData,
+    });
   }
-
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
