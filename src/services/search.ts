9038026e4d644df37047ad8ea0e9d585ef559b03
viniciuspra <vinicius.cascaesp@gmail.com>
2023-12-12 20:27:04 -0300

    feat: Add user search filter and create function to fetch user contacts

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,12 +1,36 @@
 import { prisma } from "../lib/prisma";
 
-async function searchUsers(search: string) {
+async function searchFilteredUsers(search: string, userId: string) {
   try {
     const users = await prisma.user.findMany({
       where: {
-        OR: [
-          { fullname: { contains: search, mode: "insensitive" } },
-          { username: { contains: search, mode: "insensitive" } },
+        AND: [
+          search
+            ? {
+                OR: [
+                  { username: { contains: search, mode: "insensitive" } },
+                  { fullname: { contains: search, mode: "insensitive" } },
+                ],
+              }
+            : {},
+          {
+            NOT: {
+              OR: [
+                {
+                  receivedRequests: {
+                    some: { senderId: userId, status: "pending" },
+                  },
+                },
+                {
+                  sentRequests: {
+                    some: { receiverId: userId, status: "pending" },
+                  },
+                },
+                { contacts: { some: { contactId: userId } } },
+                { contactsOf: { some: { userId } } },
+              ],
+            },
+          },
         ],
       },
       select: {
@@ -18,6 +42,75 @@ async function searchUsers(search: string) {
     });
 
     return users;
+  } catch (error) {
+    console.error("Error during user search: ", error);
+    return [];
+  }
+}
+
+async function searchUserContacts(search: string, userId: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        contacts: {
+          include: {
+            contact: true,
+          },
+          where: {
+            contact: {
+              OR: [
+                {
+                  fullname: {
+                    contains: search,
+                    mode: "insensitive",
+                  },
+                },
+                {
+                  username: {
+                    contains: search,
+                    mode: "insensitive",
+                  },
+                },
+              ],
+            },
+          },
+        },
+        contactsOf: {
+          include: {
+            user: true,
+          },
+          where: {
+            user: {
+              OR: [
+                {
+                  fullname: {
+                    contains: search,
+                    mode: "insensitive",
+                  },
+                },
+                {
+                  username: {
+                    contains: search,
+                    mode: "insensitive",
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      console.error("User not found");
+      return [];
+    }
+
+    const contacts = user.contacts.map((contactEntry) => contactEntry.contact);
+    const contactsOf = user.contactsOf.map((contactEntry) => contactEntry.user);
+
+    return [...contacts, ...contactsOf];
   } catch (error) {
     console.error("Error during user search: ", error);
     return [];
@@ -39,4 +132,4 @@ async function getUser(username: string) {
   }
 }
 
-export { searchUsers, getUser };
+export { searchFilteredUsers, searchUserContacts, getUser };
