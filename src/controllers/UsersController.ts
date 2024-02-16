54d5e1ffe36aebbaab4a089ac4b0ba0f22e32ab6
viniciuspra <vinicius.cascaesp@gmail.com>
2024-02-16 16:42:57 -0300

    Update UsersController.ts

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from "express";
 import { AppError } from "../utils/AppError";
 import { prisma } from "../lib/prisma";
 import * as bcrypt from "bcrypt";
