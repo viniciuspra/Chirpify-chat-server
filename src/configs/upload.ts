7816e84cb82bad96bc31c4eb59d258b0428350c6
viniciuspra <vinicius.cascaesp@gmail.com>
2024-02-24 15:54:31 -0300

    feat: Update path for tmp folder

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 import path from "node:path";
 import crypto from "node:crypto";
 
-const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const TMP_FOLDER = path.resolve(__dirname, "tmp");
 const UPLOAD_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER: multer.Options = {
